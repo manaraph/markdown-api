cebb0d348a70d7865ad37a161fb98129883fd60a
Manasseh Omachonu <manaraph225@gmail.com>
2019-04-24 00:31:06 +0100

    Develop skeleton

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
 
 app.get('/', function(req, res){
     res.send("Welcome to my markdown API")
 })
 
-app.listen(3000);
+app.post('/login', function(req, res){
+    res.send("Authenticated");
+})
+
+app.post('/convert', function(req, res){
+    console.log(req.body);
+
+    if(typeof req.body.content == 'undefined' || req.body.content == null){
+        res.json(["error", "No data found"]);
+    }else{
+        res.json(["markdown", req.body.content]);
+    }
+    
+})
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000")
+});
