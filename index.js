6cd6c54c869dc61aa9cd320a1a75f40a644d784e
Manasseh Omachonu <manaraph225@gmail.com>
2019-04-24 00:42:01 +0100

    Add nodemon and fix errors so far

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const showdown = require('showdown');
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
+
+converter = new showdown.Converter()
 
 app.get('/', function(req, res){
     res.send("Welcome to my markdown API")
@@ -13,13 +16,15 @@ app.post('/login', function(req, res){
     res.send("Authenticated");
 })
 
-app.post('/convert', function(req, res){
+app.post('/convert', function(req, res, next){
     console.log(req.body);
 
     if(typeof req.body.content == 'undefined' || req.body.content == null){
         res.json(["error", "No data found"]);
     }else{
-        res.json(["markdown", req.body.content]);
+        text = req.body.content;
+        html = converter.makeHtml(text);
+        res.json(["markdown", html]);
     }
     
 })
